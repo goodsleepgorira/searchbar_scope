f99a595441a85b35135d6d8f707630fbcedfb85e
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-06-05 22:54:57 -0400

    Test

--- SearchBarTest/AppDelegate.swift
+++ SearchBarTest/AppDelegate.swift
@@ -1,9 +1,5 @@
 //
 //  AppDelegate.swift
-//  SearchBarTest
-//
-//  Created by 齋藤緒 on 2016/06/06.
-//  Copyright © 2016年 TestOrganization. All rights reserved.
 //
 
 import UIKit
--- SearchBarTest/ViewController.swift
+++ SearchBarTest/ViewController.swift
@@ -1,25 +1,55 @@
 //
 //  ViewController.swift
-//  SearchBarTest
-//
-//  Created by 齋藤緒 on 2016/06/06.
-//  Copyright © 2016年 TestOrganization. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var testTableView: UITableView!
+    
+    //データ
+    let dataList = ["月刊コロコロコミック（小学館）",
+        "コロコロイチバン！（小学館）",
+        "最強ジャンプ（集英社）",
+        "Vジャンプ（集英社）",
+        "週刊少年サンデー（小学館）",
+        "週刊少年マガジン（講談社）",
+        "週刊少年ジャンプ（集英社）",
+        "週刊少年チャンピオン（秋田書店）",
+        "月刊少年マガジン（講談社）",
+        "月刊少年チャンピオン（秋田書店）",
+        "月刊少年ガンガン（スクウェア）", 
+        "月刊少年エース（KADOKAWA）",
+        "月刊少年シリウス（講談社）",
+        "週刊ヤングジャンプ（集英社）",
+        "ビッグコミックスピリッツ（小学館）",
+        "週刊ヤングマガジン（講談社）"]
+    
+    
+    //最初からあるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    //データを返すメソッド
+    func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        
+        //セルを取得する。
+        let cell = tableView.dequeueReusableCellWithIdentifier("TestCell", forIndexPath:indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = dataList[indexPath.row]
+        return cell
     }
-
+    
+    
+    
+    //データの個数を返すメソッド
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return dataList.count
+    }
 
 }
 
