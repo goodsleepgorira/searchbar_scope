66ef193f1fd4cf7e0c0f408cb5871c6acce497cd
goodsleepgorira <goodsleepgorira@yahoo.co.jp>
2016-06-06 01:51:11 -0800

    Test

--- SearchBarTest/ViewController.swift
+++ SearchBarTest/ViewController.swift
@@ -4,9 +4,10 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
-
+class ViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate{
+    
     @IBOutlet weak var testTableView: UITableView!
+    @IBOutlet weak var testSearchBar: UISearchBar!
     
     //データ
     let dataList = ["月刊コロコロコミック（小学館）",
@@ -19,17 +20,29 @@ class ViewController: UIViewController, UITableViewDataSource {
         "週刊少年チャンピオン（秋田書店）",
         "月刊少年マガジン（講談社）",
         "月刊少年チャンピオン（秋田書店）",
-        "月刊少年ガンガン（スクウェア）", 
+        "月刊少年ガンガン（スクウェア）",
         "月刊少年エース（KADOKAWA）",
         "月刊少年シリウス（講談社）",
         "週刊ヤングジャンプ（集英社）",
         "ビッグコミックスピリッツ（小学館）",
         "週刊ヤングマガジン（講談社）"]
     
+    //検索結果配列
+    var searchResult = [String]()
+    
     
     //最初からあるメソッド
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //デリゲート先を自分に設定する。
+        testSearchBar.delegate = self
+        
+        //何も入力されていなくてもReturnキーを押せるようにする。
+        testSearchBar.enablesReturnKeyAutomatically = false
+        
+        //検索結果配列にデータをコピーする。
+        searchResult = dataList
     }
     
     
@@ -39,8 +52,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         //セルを取得する。
         let cell = tableView.dequeueReusableCellWithIdentifier("TestCell", forIndexPath:indexPath) as UITableViewCell
-        
-        cell.textLabel?.text = dataList[indexPath.row]
+        cell.textLabel?.text = searchResult[indexPath.row]
         return cell
     }
     
@@ -48,8 +60,37 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     //データの個数を返すメソッド
     func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
-        return dataList.count
+        return searchResult.count
     }
-
+    
+    
+    
+    //検索ボタン押下時の呼び出しメソッド
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        //キーボードを閉じる。
+        testSearchBar.endEditing(true)
+    }
+    
+    
+    //テキスト変更時の呼び出しメソッド
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        //検索結果配列を空にする。
+        searchResult.removeAll()
+        
+        if(testSearchBar.text == "") {
+            //検索文字列が空の場合はすべてを表示する。
+            searchResult = dataList
+        } else {
+            //検索文字列を含むデータを検索結果配列に追加する。
+            for data in dataList {
+                if data.containsString(testSearchBar.text!) {
+                    searchResult.append(data)
+                }
+            }
+        }
+        
+        //テーブルを再読み込みする。
+        testTableView.reloadData()
+    }
+    
 }
-
